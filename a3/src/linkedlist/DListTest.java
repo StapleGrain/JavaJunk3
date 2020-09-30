39e8730cf195208cf3f12da0d7df99ded487fc1b
StapleGrain <63561992+StapleGrain@users.noreply.github.com>
2020-09-30 13:32:23 -0400

    :

--- a3/src/linkedlist/DListTest.java
+++ a3/src/linkedlist/DListTest.java
@@ -1,14 +1,39 @@
 package linkedlist;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 
 class DListTest {
 
     @Test
-    void test() {
+    // tests toStringR and append
+    void test1and2() {
+        // cases to test
         fail("Not yet implemented");
     }
 
+    @Test
+    // tests prepend
+    void testprepend() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    // tests getNode
+    void testgetNode() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    // tests delete
+    void testdelete() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    // tests insertAfter
+    void testinsertAfter() {
+        fail("Not yet implemented");
+    }
 }
