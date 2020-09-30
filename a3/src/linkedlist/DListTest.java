bb99488531caaa3f9de31cb1dce87a89db0e72d0
StapleGrain <63561992+StapleGrain@users.noreply.github.com>
2020-09-30 15:24:03 -0400

    Hi again

--- a3/src/linkedlist/DListTest.java
+++ a3/src/linkedlist/DListTest.java
@@ -9,29 +9,20 @@ class DListTest {
 
     @Test
     // tests toStringR and append
-    void test1and2() {
+    void testAppendAndToStringR() {
         // cases to test
         DList<Object> a= new DList<>();
+        assertEquals("[]", a.toString());
         assertEquals("[]", a.toStringR());
         assertEquals(0, a.size());
-        assertEquals(null, a.firstNode());
-        assertEquals(null, a.lastNode());
         a.append("a");
+        assertEquals("[a]", a.toString());
         assertEquals("[a]", a.toStringR());
         assertEquals(1, a.size());
-        assertEquals("a", a.first());
-        assertEquals("a", a.last());
-        assertEquals(null, a.firstNode().pred());
-        assertEquals(null, a.firstNode().succ());
         a.append(12);
+        assertEquals("[a, 12]", a.toString());
         assertEquals("[12, a]", a.toStringR());
         assertEquals(2, a.size());
-        assertEquals("a", a.first());
-        assertEquals(12, a.last());
-        assertEquals(null, a.firstNode().pred());
-        assertEquals(12, a.firstNode().succ().value());
-        assertEquals(null, a.lastNode().succ());
-        assertEquals("a", a.lastNode().pred().value());
     }
 
     // @Test
@@ -56,33 +47,28 @@ class DListTest {
         d.append("D");
         d.append("E");
         assertEquals("[A, B, C, D, E]", d.toString());
+        assertEquals("[E, D, C, B, A]", d.toStringR());
         assertEquals(5, d.size());
         d.delete(d.lastNode());
         assertEquals("[A, B, C, D]", d.toString());
+        assertEquals("[D, C, B, A]", d.toStringR());
         assertEquals(4, d.size());
-        assertEquals("D", d.last());
-        assertEquals(null, d.lastNode().succ());
         d.delete(d.firstNode());
         assertEquals("[B, C, D]", d.toString());
+        assertEquals("[D, C, B]", d.toStringR());
         assertEquals(3, d.size());
-        assertEquals("B", d.first());
-        assertEquals(null, d.firstNode().pred());
         d.delete(d.firstNode().succ());
         assertEquals("[B, D]", d.toString());
+        assertEquals("[D, B]", d.toStringR());
         assertEquals(2, d.size());
-        assertEquals("D", d.firstNode().succ().value());
-        assertEquals("B", d.lastNode().pred().value());
         d.delete(d.lastNode());
         assertEquals("[B]", d.toString());
+        assertEquals("[B]", d.toStringR());
         assertEquals(1, d.size());
-        assertEquals(null, d.firstNode().succ());
-        assertEquals(null, d.lastNode().pred());
-        assertEquals("B", d.last());
         d.delete(d.lastNode());
         assertEquals("[]", d.toString());
+        assertEquals("[]", d.toStringR());
         assertEquals(0, d.size());
-        assertEquals(null, d.firstNode());
-        assertEquals(null, d.lastNode());
     }
 
     // @Test
